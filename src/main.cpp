68af465883d2b0ab7cc357a25833f2518049c990
Mathews_Vinoy <mathewsvinoy@gmail.com>
2026-08-04 19:15:01 +0000

    config set

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,36 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <ctime>
-#include "grid.cpp"
 #include "raylib.h"
-using namespace std;
+#include "grid.cpp"
 
 int main() {
     Grids grd;
     grd.randomGrid();
-    for (int g = 0; g < 10; g++) {
-        cout << "Generation: " << g + 1 << "\n";
-        grd.display();
+    const int screenWidth = 500;
+    const int screenHeight = 500;
+    const int CELL_SIZE = 20;
+    InitWindow(screenWidth, screenHeight, "Game of Life");
+	SetTargetFPS(10);
+
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        for (int i = 0; i < grd.height; i++) {
+            for (int j = 0; j < grd.width; j++) {
+                 Rectangle cellRect = {
+                    (float)(j * CELL_SIZE),
+                    (float)(i * CELL_SIZE),
+                    (float)CELL_SIZE,
+                    (float)CELL_SIZE
+                };
+
+                Color cellColor = grd.grid[i][j] == 1 ? BLACK : RAYWHITE;
+                DrawRectangleRec(cellRect, cellColor);
+                DrawRectangleLinesEx(cellRect, 1, LIGHTGRAY);
+            }
+        }
         grd.evaluation();
         grd.grid.swap(grd.ngd);
+        EndDrawing();
     }
+    CloseWindow();
     return 0;
 }
